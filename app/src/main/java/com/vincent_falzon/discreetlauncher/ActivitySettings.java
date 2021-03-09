4b5c4c3be151654051590ca3f494501cd718bc14
falzonv <falzonv@users.noreply.github.com>
2021-03-09 18:16:18 +0100

    Add an icon pack selection setting

--- app/src/main/java/com/vincent_falzon/discreetlauncher/ActivitySettings.java
+++ app/src/main/java/com/vincent_falzon/discreetlauncher/ActivitySettings.java
@@ -23,15 +23,26 @@ package com.vincent_falzon.discreetlauncher;
  */
 
 // Imports
+import android.content.Intent ;
+import android.content.pm.PackageManager ;
+import android.content.pm.ResolveInfo ;
 import android.os.Bundle ;
 import androidx.appcompat.app.AppCompatActivity ;
+import androidx.preference.ListPreference ;
 import androidx.preference.PreferenceFragmentCompat ;
+import java.util.ArrayList ;
+import java.util.List ;
 
 /**
  * Settings and Help activity.
  */
 public class ActivitySettings extends AppCompatActivity
 {
+	// Attributes
+	private static ArrayList<String> global_iconPacks;
+	private static ArrayList<String> global_packsNames ;
+
+
 	/**
 	 * Load the settings from the XML file and prepare their values.
 	 */
@@ -47,6 +58,14 @@ public class ActivitySettings extends AppCompatActivity
 		{
 			// Load the settings from the XML file
 			setPreferencesFromResource(R.xml.settings, rootKey) ;
+
+			// Initialize the icon pack selector
+			ListPreference iconPack = findPreference("icon_pack") ;
+			if(iconPack != null)
+				{
+					iconPack.setEntries(global_packsNames.toArray(new CharSequence[0]));
+					iconPack.setEntryValues(global_iconPacks.toArray(new CharSequence[0]));
+				}
 		}
 	}
 
@@ -61,8 +80,40 @@ public class ActivitySettings extends AppCompatActivity
 		// Call the constructor of the parent class
 		super.onCreate(savedInstanceState) ;
 
+		// Search for installed icon packs
+		searchIconPacks() ;
+
 		// Load the settings from the XML file
 		setContentView(R.layout.activity_settings) ;
 		getSupportFragmentManager().beginTransaction().replace(R.id.settings_container, new SettingsFragment()).commit() ;
+	}
+
+
+	/**
+	 * Build a list of the installed icon packs
+	 */
+	private void searchIconPacks()
+	{
+		// Initializations
+		if(global_iconPacks == null) global_iconPacks = new ArrayList<>() ;
+			else global_iconPacks.clear() ;
+		if(global_packsNames == null) global_packsNames = new ArrayList<>() ;
+			else global_packsNames.clear() ;
+		PackageManager apkManager = getPackageManager() ;
+		Intent filter = new Intent("org.adw.launcher.THEMES") ;
+
+		// Add the default option first (no icon pack)
+		global_iconPacks.add("none") ;
+		global_packsNames.add(getString(R.string.text_no_icon_pack)) ;
+
+		// Retrieve the list of installed icon packs
+		List<ResolveInfo> packsList = apkManager.queryIntentActivities(filter, PackageManager.GET_META_DATA) ;
+
+		// Browse the retrieved packs and store their information in the lists
+		for(ResolveInfo pack:packsList)
+		{
+			global_iconPacks.add(pack.activityInfo.packageName) ;
+			global_packsNames.add(pack.loadLabel(apkManager).toString()) ;
+		}
 	}
 }
