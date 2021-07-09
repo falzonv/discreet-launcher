1250957022add05b3dc773a3e5ccaf702a589476
falzonv <falzonv@users.noreply.github.com>
2021-07-09 18:15:11 +0200

    Use arrays for clock format and theme

--- app/src/main/java/com/vincent_falzon/discreetlauncher/settings/ActivitySettings.java
+++ app/src/main/java/com/vincent_falzon/discreetlauncher/settings/ActivitySettings.java
@@ -158,44 +158,6 @@ public class ActivitySettings extends AppCompatActivity
 					iconPack.setEntries(packsNames.toArray(new CharSequence[0])) ;
 					iconPack.setEntryValues(iconPacks.toArray(new CharSequence[0])) ;
 				}
-
-			// Prepare the theme options
-			ArrayList<String> themes = new ArrayList<>() ;
-			ArrayList<String> themesNames = new ArrayList<>() ;
-			themesNames.add(getString(R.string.set_application_theme_light)) ;
-			themes.add("light") ;
-			themesNames.add(getString(R.string.set_application_theme_dark)) ;
-			themes.add("dark") ;
-			themesNames.add(getString(R.string.set_application_theme_system)) ;
-			themes.add(Constants.NONE) ;
-
-			// Prepare the clock formats
-			ArrayList<String> formats = new ArrayList<>() ;
-			ArrayList<String> formatsNames = new ArrayList<>() ;
-			formatsNames.add(getString(R.string.set_clock_format_none)) ;
-			formats.add(Constants.NONE) ;
-			formatsNames.add(getString(R.string.set_clock_format_24h)) ;
-			formats.add("HH:mm") ;
-			formatsNames.add(getString(R.string.set_clock_format_12h)) ;
-			formats.add("h:mm") ;
-			formatsNames.add(getString(R.string.set_clock_format_12h_ampm)) ;
-			formats.add("h:mm a") ;
-
-			// Initialize the theme selector
-			ListPreference theme = findPreference(Constants.APPLICATION_THEME) ;
-			if(theme != null)
-				{
-					theme.setEntries(themesNames.toArray(new CharSequence[0])) ;
-					theme.setEntryValues(themes.toArray(new CharSequence[0])) ;
-				}
-
-			// Initialize the clock format selector
-			ListPreference clockFormat = findPreference(Constants.CLOCK_FORMAT) ;
-			if(clockFormat != null)
-				{
-					clockFormat.setEntries(formatsNames.toArray(new CharSequence[0])) ;
-					clockFormat.setEntryValues(formats.toArray(new CharSequence[0])) ;
-				}
 		}
 	}
 
