b2e359150dfd3c6eb1569bbfe2d4c7286dfae513
falzonv <falzonv@users.noreply.github.com>
2021-09-05 18:36:58 +0200

    Create a dedicated Help activity

--- app/src/main/java/com/vincent_falzon/discreetlauncher/settings/ActivitySettings.java
+++ app/src/main/java/com/vincent_falzon/discreetlauncher/settings/ActivitySettings.java
@@ -184,26 +184,6 @@ public class ActivitySettings extends AppCompatActivity
 
 
 	/**
-	 * Load the help from the XML file.
-	 */
-	@SuppressWarnings({"unused", "RedundantSuppression"})
-	public static class HelpFragment extends PreferenceFragmentCompat
-	{
-		/**
-		 * Constructor.
-		 * @param savedInstanceState To retrieve the context
-		 * @param rootKey Root of the settings hierarchy
-		 */
-		@Override
-		public void onCreatePreferences(Bundle savedInstanceState, String rootKey)
-		{
-			// Load the settings from the XML file
-			setPreferencesFromResource(R.xml.settings_help, rootKey) ;
-		}
-	}
-
-
-	/**
 	 * Build a list of the installed icon packs.
 	 */
 	private void searchIconPacks()
