e1fc309b56e7105f1464b86b4b424e28ac89454e
falzonv <falzonv@users.noreply.github.com>
2023-09-05 15:07:02 +0200

    Rollback list update on package changes

--- app/src/main/java/com/vincent_falzon/discreetlauncher/events/PackagesListener.java
+++ app/src/main/java/com/vincent_falzon/discreetlauncher/events/PackagesListener.java
@@ -38,9 +38,6 @@ public class PackagesListener extends BroadcastReceiver
 	// Constants
 	private static final String TAG = "PackagesListener" ;
 
-	// Attributes
-	private int last_app_id = -1 ;
-
 
 	/**
 	 * Provide the filter to use when registering this receiver.
@@ -49,7 +46,6 @@ public class PackagesListener extends BroadcastReceiver
 	{
 		IntentFilter filter = new IntentFilter() ;
 		filter.addAction(Intent.ACTION_PACKAGE_ADDED) ;
-		filter.addAction(Intent.ACTION_PACKAGE_CHANGED) ;
 		filter.addAction(Intent.ACTION_PACKAGE_REMOVED) ;
 		filter.addDataScheme("package") ;
 		return filter ;
@@ -62,27 +58,16 @@ public class PackagesListener extends BroadcastReceiver
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-		// Do not continue if the intent is invalid
+		// Check if the intent is valid
 		if(intent == null) return ;
-		String action = intent.getAction() ;
-		if(!Intent.ACTION_PACKAGE_ADDED.equals(action) &&
-				!Intent.ACTION_PACKAGE_CHANGED.equals(action) &&
-				!Intent.ACTION_PACKAGE_REMOVED.equals(action)) return ;
-
-		// Do not handle the same event twice from different intents
-		int app_id = intent.getIntExtra(Intent.EXTRA_UID, -1) ;
-		Utils.logDebug(TAG, "received Intent{" + action + ", app=" + app_id + "}") ;
-		if(!action.equals(Intent.ACTION_PACKAGE_CHANGED)) last_app_id = app_id ;
-			else if(last_app_id == app_id)
-			{
-				last_app_id = -1 ;
-				return ;
-			}
+		Utils.logDebug(TAG, "received " + intent) ;
 
 		// Do not react to applications updates
 		if(intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)) return ;
 
-		// Update the list of applications
-		updateList(context) ;
+		// If a package has been added or removed, update the list of applications
+		String action = intent.getAction() ;
+		if(Intent.ACTION_PACKAGE_ADDED.equals(action) || Intent.ACTION_PACKAGE_REMOVED.equals(action))
+			updateList(context) ;
 	}
 }
