1c49ad0a41041d23820008e59e45759eee67708b
falzonv <falzonv@users.noreply.github.com>
2025-01-04 21:42:38 +0100

    Update the copyright date to 2025

--- app/src/main/java/com/vincent_falzon/discreetlauncher/quickaccess/ShortcutCreator.java
+++ app/src/main/java/com/vincent_falzon/discreetlauncher/quickaccess/ShortcutCreator.java
@@ -5,7 +5,7 @@ package com.vincent_falzon.discreetlauncher.quickaccess ;
 
 	This file is part of Discreet Launcher.
 
-	Copyright (C) 2019-2024 Vincent Falzon
+	Copyright (C) 2019-2025 Vincent Falzon
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
